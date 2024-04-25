767f6bde704423d8ac4a3d5471bb6fbd79c2318b
mariajvieira <up202204802@up.pt>
2024-04-25 22:24:26 +0100

    Improved login screen

--- projeto/lib/screens/login_screen.dart
+++ projeto/lib/screens/login_screen.dart
@@ -30,9 +30,12 @@ class _LoginState extends State<Login> {
     if (isValidUser) {
       _navigateToGeoLocationScreen();
     } else {
-      if (!username.text.endsWith('@up.pt')) {
+      if (username.text.isEmpty || password.text.isEmpty){
+        errorMessage = "Username and password required";
+      } else if (!username.text.endsWith('@up.pt')) {
         errorMessage = "Invalid username";  // Specific message for username validation
-      } else {
+      }
+      else {
         errorMessage = "Username or password is incorrect";  // General error message
       }
       setState(() {
@@ -56,58 +59,149 @@ class _LoginState extends State<Login> {
         child: SingleChildScrollView(  // Use SingleChildScrollView to avoid overflow when keyboard appears
           child: Form(
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                if (isLoginFailed)
-                  Text(
-                    errorMessage,
-                    style: const TextStyle(color: Colors.red, fontSize: 14),
-                  ),
-                TextFormField(
-                  controller: username,
-                  decoration: InputDecoration(
-                    labelText: 'Username',
-                    hintText: 'Enter your UP email',
-                    filled: true, // new line
-                    fillColor: Colors.white, // new line
-                    border: OutlineInputBorder( // new line
-                      borderRadius: BorderRadius.circular(8), // new line
-                      borderSide: BorderSide.none, // new line
+              children: [
+                Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget> [
+                    const SizedBox(height: 20.0,),
+                    const Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget> [
+                          Text('CleanIt',
+                            style: TextStyle(
+                              color: Colors.blueAccent,
+                              letterSpacing: 2.0,
+                              fontSize: 50.0,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Roboto-Bold',
+                            ),
+                          ),
+                          Text('UP',
+                            style: TextStyle(
+                              color: Colors.green,
+                              letterSpacing: 2.0,
+                              fontSize: 50.0,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Roboto-Bold',
+                            ),
+                          )
+                        ]
                     ),
-                  ),
-                ),
-                const SizedBox(height: 8), // Add some space between fields
-                TextFormField(
-                  controller: password,
-                  obscureText: !isVisible,
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    hintText: 'Enter your password',
-                    filled: true, // new line
-                    fillColor: Colors.white, // new line
-                    border: OutlineInputBorder( // new line
-                      borderRadius: BorderRadius.circular(8), // new line
-                      borderSide: BorderSide.none, // new line
+                    const SizedBox(height: 10.0,),
+                    const Text('Login with up email',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Roboto-Bold',
+                      ),),
+
+
+                      const SizedBox(height: 10.0),
+                      TextFormField(
+                        controller: username,
+                        decoration: InputDecoration(
+                          fillColor: Colors.white,
+                          filled: true,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide: BorderSide.none,
+                          ),
+                          hintText: 'up*********@up.pt',
+                        ),
+                      ),
+                      const SizedBox(height: 20.0), // Add some space between fields
+                      TextFormField(
+                        controller: password,
+                        obscureText: !isVisible,
+                        decoration: InputDecoration(
+                            fillColor: Colors.white,
+                            filled: true,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                              borderSide: BorderSide.none,
+                            ),
+                            hintText: 'password',
+                            suffixIcon: IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  isVisible = !isVisible;
+                                });
+                              }, icon: Icon(isVisible? Icons.visibility : Icons.visibility_off),
+                            )
+                        ),
+                      ),
+                    const SizedBox(height: 10.0,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget> [
+                        Text('By clicking continue, you agree to our ',
+                          style: TextStyle(
+                            color: Colors.grey[500],
+                            fontFamily: 'Roboto-Regular',
+                          ),
+                        ),
+                        Text('Terms of Service ',
+                          style: TextStyle(
+                            color: Colors.grey[500],
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Roboto-Regular',
+                          ),)
+                      ],
                     ),
-                    suffixIcon: IconButton(
-                      icon: Icon(isVisible ? Icons.visibility : Icons.visibility_off),
-                      onPressed: () {
-                        setState(() {
-                          isVisible = !isVisible;
-                        });
-                      },
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text('and',
+                          style: TextStyle(
+                            color: Colors.grey[500],
+                            fontFamily: 'Roboto-Regular',
+                          ),),
+                        Text(' Privacy Policy',
+                          style: TextStyle(
+                            color: Colors.grey[500],
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Roboto-Regular',
+                          ),)
+                      ],
                     ),
-                  ),
+                    if (isLoginFailed)
+                      Padding(
+                          padding: EdgeInsets.symmetric(vertical: 10.0),
+                          child: Text(
+                            errorMessage,
+                            style: const TextStyle(color: Colors.red, fontSize: 14),
+                          ),
+                      )
+
+                  ],
                 ),
-                ElevatedButton(
-                  onPressed: login,
-                  child: const Text('LOGIN'),
                 ),
+
               ],
             ),
           ),
         ),
       ),
+        floatingActionButton: Stack(
+          alignment: Alignment.bottomRight,
+          children: [
+            Positioned(
+              bottom: 40,
+              right: 20,
+              left: 50,
+              child: FloatingActionButton(
+                onPressed: login,
+                tooltip: 'Increment',
+                backgroundColor: Colors.white,
+                elevation: 6,
+                child: const Text('LOGIN'),
+              ),
+            )
+          ],
+        )
     );
   }
 }
