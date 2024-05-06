438c1245e0a0568ee04f99445ed00e7cef39f510
lordnazeem <166418597+lordnazeem@users.noreply.github.com>
2024-05-06 16:05:56 +0300

    Add files via upload
    Made up the shortage with calendar

--- projeto/lib/screens/calendar_screen.dart
+++ projeto/lib/screens/calendar_screen.dart
@@ -1,63 +1,134 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-class CalendarScreen extends StatelessWidget {
-  
-  
+class CalendarScreen extends StatefulWidget {
   const CalendarScreen({Key? key}) : super(key: key);
 
-
-
   @override
-  Widget build(BuildContext context) {
+  _CalendarScreenState createState() => _CalendarScreenState();
+}
+
+  class _CalendarScreenState extends State<CalendarScreen>{
+    
+    String dailyAction = "";
+
+    List<String> achievementIcons = [
+      '05.05.2024',
+      '06.05.2024',
+      '07.05.2024',
+      '08.05.2024',
+      '09.05.2024',
+      '10.05.2024',
+    ];
+
+    List<String> dailyActivities = [
+      "Some activity 1",
+      "Some activity 2",
+      "Some activity 3",
+      "Some activity 4",
+      "Some activity 5",
+      "Some activity 6",
+      "Some activity 7",
+    ];
+    @override
+    Widget build(BuildContext context) {
+    
+
+    void updateSelectedItem(String item) {
+    setState(() {
+      dailyAction = item;
+      print("Daily action updated to: $dailyAction");
+    });
+  }
+
+
     return Scaffold(
       body: Container(
         padding: const EdgeInsets.all(16),
-        alignment: Alignment.center,
         child: SingleChildScrollView(
           child: Column(
             children: <Widget>[
-              const SizedBox(height: 60.0),
+              const SizedBox(height: 24.0),
               const Text(
                 'Upcoming Sustainability Activities',
                 style: TextStyle(
-                  color: Colors.black,
-                  letterSpacing: 2.0,
-                  fontSize: 30.0,
-                  fontWeight: FontWeight.bold
-                ),
+                    color: Colors.black,
+                    letterSpacing: 2.0,
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 20.0),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: Text(
+                      'Date',
+                      style: TextStyle(
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Wrap(
+                    children: [
+                      Container(
+                        height: 150.0,
+                        child: ListView.separated(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: achievementIcons.length,
+                          separatorBuilder: (context, index) =>
+                              SizedBox(width: 10),
+                          itemBuilder: (context, index) {
+                            return TextButton(
+                              onPressed: () => {
+                                updateSelectedItem(dailyActivities[index])
+                              },
+                              child: Container(
+                                  padding: EdgeInsets.all(10.0),
+                                  child: 
+                                      Text(
+                                        achievementIcons[index],
+                                        selectionColor: Colors.black,
+                                        style: TextStyle(
+                                            fontSize: 24.0,
+                                            color: Colors.black,
+                                            fontWeight: FontWeight.w700,
+                                            backgroundColor: Colors.blue),
+                                      ),
+                                ),
+                              );
+                            
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
 
-            SizedBox(
-              height: 200,
-              child: CupertinoDatePicker(
-                mode: CupertinoDatePickerMode.dateAndTime,
-                initialDateTime: DateTime(1969, 1, 1, 11, 33),
-                onDateTimeChanged: (DateTime newDateTime) {
-                  //Do Some thing
-                },
-                use24hFormat: false,
-                minuteInterval: 1,
+              
+              Text(
+                dailyAction,
+                style : TextStyle(
+                  color : Colors.black
+                )
               ),
-            ),
-              
-              _buildAchievementsSection(),
-              
+
+              const SizedBox(height: 200.0),
+
               ElevatedButton(
                 onPressed: () {
                   ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                        content: Text("Events seen succesfully."),
-                    )); 
+                    content: Text("You are joined to this event."),
+                  ));
                 },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.red
-                ),
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
                 child: const Text(
-                  'Set all events seen',
-                  style: TextStyle(
-                    color: Colors.white
-                  ),
-                  ),
+                  'Participate in this event.',
+                  style: TextStyle(color: Colors.white),
+                ),
               ),
             ],
           ),
@@ -66,82 +137,3 @@ class CalendarScreen extends StatelessWidget {
     );
   }
 }
-
-
-
-  Widget _buildAchievementsSection() {
-    // part of achievements
-    List<String> achievementIcons = [
-      //acrescentar
-      'assets/icon.png',
-      'assets/icon_.png',
-      'assets/icon_.png',
-      'assets/icon_.png',
-      'assets/icon_.png',
-      'assets/icon_.png',
-    ];
-
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8.0),
-          child: Text(
-            'My events',
-            style: TextStyle(
-              fontSize: 24.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-        Wrap(
-          children: [Container(
-            height: 350.0,
-          child: ListView.separated(
-            scrollDirection: Axis.vertical,
-            itemCount: achievementIcons.length,
-            separatorBuilder: (context, index) => SizedBox(width: 10),
-            itemBuilder: (context, index) {
-              return Container(
-                padding: EdgeInsets.all(10),
-                
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget> [
-                      const Text(
-                        "Storm",
-                        style: TextStyle(
-                          fontSize: 24.0,
-                          color: Colors.black,
-                          fontWeight: FontWeight.w700,
-                          backgroundColor: Colors.blue
-                        ),
-                      ),
-                    
-                    const Text(
-                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-                        style: TextStyle(
-                          fontSize: 16.0,
-                          color: Color.fromARGB(255, 114, 114, 114),
-                          fontWeight: FontWeight.w400
-                        ),
-                      ),
-
-                  const SizedBox(height: 5.0),
-
-                  const Divider(height: 2.0)
-
-                  ],
-
-                )
-              );
-            },
-          ),
-        ),
-       ],
-        ),
-         
-      ],
-    );
-  }
-
