aeffa451a0755cbe61efde3414088083f8fc8aea
lordnazeem <166418597+lordnazeem@users.noreply.github.com>
2024-05-10 21:07:20 +0200

    Update calendar_screen.dart
    All issues with calendar screen are solved

--- projeto/lib/screens/calendar_screen.dart
+++ projeto/lib/screens/calendar_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-
 class CalendarScreen extends StatefulWidget {
   const CalendarScreen({Key? key}) : super(key: key);
 
@@ -12,137 +11,159 @@ class CalendarScreen extends StatefulWidget {
 class _CalendarScreenState extends State<CalendarScreen> {
   String selectedDate = ""; // Store the selected date
 
-  List<String> achievementIcons = [
-    '05.05.2024',
-    '06.05.2024',
-    '07.05.2024',
-    '08.05.2024',
-    '09.05.2024',
-    '10.05.2024',
-  ];
+  Map<String, List<String>> events = {
+    '02.05.2024': ["Some activity 1"],
+    '08.05.2024': ["Some activity 2"],
+    '16.05.2024': ["Some activity 3"],
+    '22.05.2024': ["Some activity 4"],
+    '28.05.2024': ["Some activity 5"],
+    '03.06.2024': ["Some activity 6", "Some activity 7"],
+  };
 
-  List<String> dailyActivities = [
-    "Some activity 1",
-    "Some activity 2",
-    "Some activity 3",
-    "Some activity 4",
-    "Some activity 5",
-    "Some activity 6",
-    "Some activity 7",
-  ];
+  List<MapEntry<String, String>> getUpcomingEvents() {
+    List<MapEntry<String, String>> upcomingEvents = [];
+    var now = DateTime.now();
+    events.forEach((key, value) {
+      var dateComponents = key.split('.');
+      var eventDate = DateTime(
+          int.parse(dateComponents[2]), int.parse(dateComponents[1]),
+          int.parse(dateComponents[0]));
+      if (eventDate.isAfter(now) || eventDate.isAtSameMomentAs(now)) {
+        upcomingEvents.add(MapEntry(key, value.join(", ")));
+      }
+    });
+    upcomingEvents.sort((a, b) {
+      var aDate = DateTime.parse("${a.key
+          .split('.')
+          .reversed
+          .join('-')}T00:00:00Z");
+      var bDate = DateTime.parse("${b.key
+          .split('.')
+          .reversed
+          .join('-')}T00:00:00Z");
+      return aDate.compareTo(bDate);
+    });
+    return upcomingEvents.take(2).toList();
+  }
 
   @override
   Widget build(BuildContext context) {
+    List<MapEntry<String, String>> upcomingEvents = getUpcomingEvents();
+
     return Scaffold(
       body: Container(
         padding: const EdgeInsets.all(16),
-        child: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              const SizedBox(height: 24.0),
-              const Text(
-                'Upcoming Sustainability Activities',
-                style: TextStyle(
-                    color: Colors.black,
-                    letterSpacing: 2.0,
-                    fontSize: 24.0,
-                    fontWeight: FontWeight.bold),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const SizedBox(height: 24.0),
+            const Text(
+              'Upcoming Sustainability Activities',
+              style: TextStyle(
+                color: Colors.black,
+                letterSpacing: 2.0,
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
               ),
-              const SizedBox(height: 20.0),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 8.0),
-                    child: Text(
-                      'Date',
-                      style: TextStyle(
-                        fontSize: 24.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  Wrap(
-                    children: [
-                      Container(
-                        height: 150.0,
-                        child: ListView.separated(
-                          scrollDirection: Axis.horizontal,
-                          itemCount: achievementIcons.length,
-                          separatorBuilder: (context, index) => SizedBox(width: 10),
-                          itemBuilder: (context, index) {
-                            return TextButton(
-                              onPressed: () {
-                                setState(() {
-                                  selectedDate = achievementIcons[index];
-                                });
-                              },
-                              child: Container(
-                                padding: EdgeInsets.all(10.0),
-                                child: Text(
-                                  achievementIcons[index],
-                                  style: TextStyle(
-                                    fontSize: 24.0,
-                                    color: selectedDate == achievementIcons[index] ? Colors.white : Colors.black,
-                                    fontWeight: FontWeight.w700,
-                                    backgroundColor: selectedDate == achievementIcons[index] ? Colors.blue : null,
-                                  ),
-                                ),
-                              ),
-                            );
-                          },
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
+            ),
+            const SizedBox(height: 20.0),
+            Text(
+              'Selected Date: $selectedDate',
+              style: TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
               ),
-              const SizedBox(height: 20.0),
+            ),
+            const SizedBox(height: 20.0),
+            if (selectedDate.isNotEmpty)
               Text(
-                'Selected Date: $selectedDate',
+                events[selectedDate]?.join(", ") ?? "No events on this date",
                 style: TextStyle(
                   fontSize: 18.0,
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              const SizedBox(height: 20.0),
-              ElevatedButton(
-                onPressed: () async {
-                  final DateTime? picked = await showDatePicker(
-                    context: context,
-                    initialDate: DateTime.now(),
-                    firstDate: DateTime(2020),
-                    lastDate: DateTime(2025),
+            const SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () async {
+                final DateTime? picked = await showDatePicker(
+                  context: context,
+                  initialDate: DateTime.now(),
+                  firstDate: DateTime(2020),
+                  lastDate: DateTime(2025),
+                );
+                if (picked != null) {
+                  setState(() {
+                    selectedDate =
+                    "${picked.day.toString().padLeft(2, '0')}.${picked.month
+                        .toString().padLeft(2, '0')}.${picked.year}";
+                  });
+                }
+              },
+              child: const Text('Pick a Date'),
+            ),
+            const SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  selectedDate = "";
+                });
+              },
+              child: const Text('Reset'),
+            ),
+            const SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                  content: Text("You are joined to this event."),
+                ));
+              },
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+              child: const Text(
+                'Participate in this event.',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            const SizedBox(height: 20.0),
+            Text(
+              'Next Events:',
+              style: TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 10.0),
+            Expanded(
+              child: ListView.builder(
+                itemCount: upcomingEvents.length,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        selectedDate = upcomingEvents[index].key;
+                      });
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: Text(
+                        '${upcomingEvents[index].value} (${upcomingEvents[index]
+                            .key})',
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          color: selectedDate == upcomingEvents[index].key
+                              ? Colors.blue
+                              : Colors.black,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
                   );
-                  if (picked != null) {
-                    setState(() {
-                      selectedDate = "${picked.day.toString().padLeft(2, '0')}.${picked.month.toString().padLeft(2, '0')}.${picked.year}";
-                    });
-                  }
                 },
-                child: const Text('Pick a Date'),
               ),
-              const SizedBox(height: 20.0),
-              ElevatedButton(
-                onPressed: () {
-                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                    content: Text("You are joined to this event."),
-                  ));
-                },
-                style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
-                child: const Text(
-                  'Participate in this event.',
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
-    );
-  }
-}
-
     );
   }
 }
