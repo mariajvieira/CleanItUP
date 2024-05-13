8832c179eb01d773f1ca4c2daba879d9f30e3bcb
Francisca Fernandes <up202106509@up.pt>
2024-05-13 19:20:46 +0100

    implementing firebase login

--- projeto/lib/main.dart
+++ projeto/lib/main.dart
@@ -6,3 +6,28 @@ void main() => runApp(const MaterialApp(
   home: MyHomePage(title: 'Flutter Demo Home Page'),
 ));
 
+
+//com firebase
+/*
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Ensure plugin services are initialized
+  await Firebase.initializeApp();             // Initialize Firebase
+
+  runApp(MyApp());                           // Run the app
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+ */
--- projeto/lib/screens/signup_screen.dart
+++ projeto/lib/screens/signup_screen.dart
@@ -286,3 +286,266 @@ class _SignUpState extends State<SignUp> {
     );
   }
 }
+
+
+//com firebase
+/*
+class SignUp extends StatefulWidget {
+  const SignUp({super.key});
+  @override
+  State<SignUp> createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  final username = TextEditingController();
+  final password = TextEditingController();
+  final confirm_password = TextEditingController();
+  final firstName = TextEditingController();
+  final lastName = TextEditingController();
+  String errorMessage = "Username or password is incorrect";
+  bool isVisible = false;
+  bool isVisible_ = false;
+  bool isSignUpFailed = false;
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  void signup() async {
+    if (!formKey.currentState!.validate()) {
+      return;
+    }
+
+    if (username.text.isEmpty || !username.text.contains('@up.pt')) {
+      setErrorMessage("Invalid email. Use your UP email");
+      return;
+    }
+
+    if (password.text.isEmpty || confirm_password.text.isEmpty || password.text != confirm_password.text) {
+      setErrorMessage("Passwords do not match");
+      return;
+    }
+
+    try {
+      print("Attempting to create user with email: ${username.text}");
+      UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
+        email: username.text,
+        password: password.text,
+      );
+      print("User created, attempting to sign up...");
+      if (userCredential.user != null) {
+        Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const Login()));
+      }
+    } catch (FirebaseAuthException e) {
+    print('Signup error: ${e.message}');
+    setErrorMessage(e.message ?? "An error occurred. Please check logs.");
+    }
+  }
+
+  void setErrorMessage(String message) {
+    setState(() {
+      errorMessage = message;
+      isSignUpFailed = true;
+    });
+  }
+
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+          padding: const EdgeInsets.all(16),
+          alignment: Alignment.center,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("lib/assets/LoginBackground.png"),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: SingleChildScrollView(
+            child: Form(
+              key: formKey,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        const SizedBox(height: 20.0,),
+                        const Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text('CleanIt',
+                              style: TextStyle(
+                                color: Colors.blueAccent,
+                                letterSpacing: 2.0,
+                                fontSize: 50.0,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: 'Roboto-Bold',
+                              ),
+                            ),
+                            Text('UP',
+                              style: TextStyle(
+                                color: Colors.green,
+                                letterSpacing: 2.0,
+                                fontSize: 50.0,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: 'Roboto-Bold',
+                              ),
+                            )
+                          ],
+                        ),
+                        const SizedBox(height: 10.0,),
+                        const Text('Sign up with your UP email',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Roboto-Bold',
+                          ),
+                        ),
+                        TextFormField(
+                          controller: firstName,
+                          decoration: InputDecoration(
+                            hintText: 'First Name',
+                            fillColor: Colors.white,
+                            filled: true,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                              borderSide: BorderSide.none,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 10.0),
+                        TextFormField(
+                          controller: lastName,
+                          decoration: InputDecoration(
+                            hintText: 'Last Name',
+                            fillColor: Colors.white,
+                            filled: true,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                              borderSide: BorderSide.none,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 10.0),
+                        TextFormField(
+                          controller: username,
+                          decoration: InputDecoration(
+                            fillColor: Colors.white,
+                            filled: true,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                              borderSide: BorderSide.none,
+                            ),
+                            hintText: 'up*********@up.pt',
+                          ),
+                        ),
+                        const SizedBox(height: 20.0),
+                        TextFormField(
+                          controller: password,
+                          obscureText: !isVisible,
+                          decoration: InputDecoration(
+                              fillColor: Colors.white,
+                              filled: true,
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                                borderSide: BorderSide.none,
+                              ),
+                              hintText: 'Password',
+                              suffixIcon: IconButton(
+                                onPressed: () {
+                                  setState(() {
+                                    isVisible = !isVisible;
+                                  });
+                                }, icon: Icon(isVisible ? Icons.visibility : Icons.visibility_off),
+                              )
+                          ),
+                        ),
+                        const SizedBox(height: 20.0),
+                        TextFormField(
+                          controller: confirm_password,
+                          obscureText: !isVisible_,
+                          decoration: InputDecoration(
+                              fillColor: Colors.white,
+                              filled: true,
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                                borderSide: BorderSide.none,
+                              ),
+                              hintText: 'Confirm Password',
+                              suffixIcon: IconButton(
+                                onPressed: () {
+                                  setState(() {
+                                    isVisible_ = !isVisible_;
+                                  });
+                                }, icon: Icon(isVisible_ ? Icons.visibility : Icons.visibility_off),
+                              )
+                          ),
+                        ),
+                        const SizedBox(height: 10.0),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text("Already have an account?",
+                              style: TextStyle(
+                                color: Colors.grey,
+                                fontFamily: 'Roboto-Regular',
+                              ),
+                            ),
+                            TextButton(
+                                onPressed: () {
+                                  Navigator.push(context, MaterialPageRoute(builder: (context) => const Login()));
+                                },
+                                child: const Text("LOGIN",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontFamily: 'Roboto-Regular',
+                                  ),
+                                )
+                            )
+                          ],
+                        ),
+                        if (isSignUpFailed)
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 10.0),
+                            child: Text(
+                              errorMessage,
+                              style: const TextStyle(color: Colors.red, fontSize: 14),
+                            ),
+                          )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        floatingActionButton: Stack(
+          alignment: Alignment.bottomRight,
+          children: [
+            Positioned(
+              bottom: 40,
+              right: 20,
+              left: 50,
+              child: FloatingActionButton(
+                onPressed: () {
+                  if (formKey.currentState!.validate()) {
+                    signup();
+                  }
+                },
+                tooltip: 'Sign Up',
+                backgroundColor: Colors.white,
+                elevation: 6,
+                child: const Text('SIGN UP'),
+              ),
+            )
+          ],
+        )
+    );
+  }
+}
+
+ */
