16e35c0a8f64fee6f0e57ff07dcc5775f86ce552
Francisca Fernandes <up202106509@up.pt>
2024-05-13 18:48:18 +0100

    corrected navigation bar

--- projeto/lib/screens/userprofile_screen.dart
+++ projeto/lib/screens/userprofile_screen.dart
@@ -193,8 +193,7 @@ class _UserProfileState extends State<UserProfile> {
             MaterialPageRoute(builder: (context) => const ForumScreen()));
         break;
       case 1:
-        Navigator.push(context,
-            MaterialPageRoute(builder: (context) => const MapScreen()));
+            Navigator.push(context, MaterialPageRoute(builder: (context) =>MapScreen(user: widget.user)));
         break;
       case 3: // Assuming 'Calendar' is at index 3
         Navigator.push(context, MaterialPageRoute(
